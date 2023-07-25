ed8ce74400925410de556ecb3378818cb1b041f5
kdev0x <119963213+kdev0x@users.noreply.github.com>
2023-07-25 05:29:47 +0300

    Create Recipe-Finder.py

---
+++ Recipe-Finder.py
@@ -0,0 +1,38 @@
+import main
+import requests
+# get api
+def get_api():
+    global url
+    url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch"
+    # asking about user specfcation
+
+    dish_type = input('what is the dish type')
+    cuisine = input('what cousien do you whant')
+    diet = input('what diet do you want')
+    intolerances = input('do you have any intolerances')
+    includeIngredients = input('what is the includeIngredients')
+
+
+    def error_handle_get_api():
+
+
+
+     try:
+        global  querystring
+        get_api()
+        querystring = {"query": dish_type, "cuisine": cuisine, "diet": diet,
+                   "intolerances": intolerances , "includeIngredients":includeIngredients}
+     except Exception:
+        print(Exception.args)
+
+
+# api
+
+headers = {
+	"X-RapidAPI-Key": main.X_RapidAPI_Key(),
+	"X-RapidAPI-Host": main.X_RapidAPI_Host()
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+print()
+print(response.json())
